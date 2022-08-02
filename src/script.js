cfd41d6a526001f44b9f422281f93bf0b41c1766
stomporc93 <cestompor9339@gmail.com>
2022-08-02 13:19:29 -0500

    added a div, css style and js functions
    Add the div class (weather-app-format).
    
    Added some style to the ul, li, and some classes.
    
    Created the JS function for the day and time to be show on the page.

--- src/script.js
+++ src/script.js
@@ -1,15 +1,18 @@
 function formatDate(date) {
-  let hours = date.nowHours();
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = date.nowMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let dayIndex = date.nowDay();
+  let dayIndex = date.getDay();
   let days = ["sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
   let day = days[dayIndex];
   return `${day}, ${hours}:${minutes}`;
 }
-console.log(formatDate);
+
+let dateElement = document.querySelector("#date");
+let currentDate = new Date();
+dateElement.innerHTML = formatDate(currentDate);
