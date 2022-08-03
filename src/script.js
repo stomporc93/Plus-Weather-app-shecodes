291233715b290662c2fbfdefc20bd321f40c3977
stomporc93 <cestompor9339@gmail.com>
2022-08-03 13:27:01 -0500

    JS functions city and weather conditions
    Function displayCurrentWeatherConditions for the city and temperature to be displayed.
    
    function searchCity with apikey and url.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+function displayCurrentWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "d78c86e142335190499b3c3f6393038a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayCurrentWeatherCondition);
+}
+
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -10,7 +23,7 @@ function formatDate(date) {
   let dayIndex = date.getDay();
   let days = ["Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday"];
   let day = days[dayIndex];
-  return `${day}, ${hours}:${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 let dateElement = document.querySelector("#date");
